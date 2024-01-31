76afed0b5e8b3bdb2d6a0a7ce7f61d75766088e2
Pankaj Jha <NI00237@nvtpower.com>
2024-01-31 13:57:09 +0530

    Added jquery automatic cursor move

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -1,5 +1,5 @@
 class Barcode < ApplicationRecord
 	belongs_to :fa
 	
-	validates :value, presence: true, length: { maximum: 40 }, uniqueness: { scope: :fa_id, message: "must be unique for each FA" }
+	validates :value, presence: true, length: { maximum: 60 }, uniqueness: { scope: :fa_id, message: "must be unique for each FA" }
 end
--- app/models/fa.rb
+++ app/models/fa.rb
@@ -6,7 +6,7 @@ class Fa < ApplicationRecord
 
   validates :Line, presence: true
   validates :Model, presence: true
-  validates :qty, presence: true, numericality: { only_integer: true }, length: { maximum: 10 }
+  validates :qty, presence: true, numericality: { only_integer: true }
   validate :validate_qty_limit
 
   LINES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
@@ -16,9 +16,9 @@ class Fa < ApplicationRecord
 
   MODELS = ['SSE-SJB4', 'SSE-SAW5', 'SSE-SCG6', 'SSE-SCT9', 'SSE-SDF8', 'SSE-SCV2', 'SSE-SDS5', 'SSE-SFW9',
     'SSE-SGM1', 'SSE-SJJ6', 'SSE-SJJ8', 'SSE-SKG5', 'SSE-SLB2', 'SSE-SLB7', 'SSE-SKP7', 'SSE-SKP2',
-    'SSE-JJ7', 'SSE-SJA4', 'SSE-SJZ3', 'SSE-SKK6', 'SSE-SJJ6', 'SSE-SLF7', 'SSE-SKG5', 'MIX-SGK6',
-    'MIX-SJZ5', 'MIX-SHR5', 'BBK-SJV5', 'BBK-SKC1', 'BBK-SJQ5', 'BBK-SJQ6', 'BBK-SJV6', 'BBK-SJQ6',
-    'BBK-SHX4', 'BBK-SHV2', 'BBK-SHD2', 'BBK-SFC8', 'OPO-SCP5', 'OPO-SCR2', 'OPO-SDY7', 'OPO-SACJ1',
+    'SSE-JJ7', 'SSE-SJA4', 'SSE-SJZ3', 'SSE-SKK6', 'SSE-SJJ6', 'SSE-SKG5', 'MIX-SGK6',
+    'MIX-SJZ5', 'MIX-SHR5', 'MIX-SKS1', 'BBK-SJV5', 'BBK-SKC1', 'BBK-SJQ5', 'BBK-SJQ6', 'BBK-SJV6',
+    'BBK-SHX4', 'BBK-SHV2', 'BBK-SHD2', 'BBK-SFC8', 'BBK-SLF7', 'OPO-SCP5', 'OPO-SCR2', 'OPO-SDY7', 'OPO-SACJ1',
     'OPO-SGL3', 'OPO-SHC9', 'OPO-SHX3', 'OPO-SHN6', 'OPO-SHS8', 'OPO-SHV3', 'OPO-SKM1', 'OPO-SKY1',
     'OPO-SJB4', 'OPO-SEC3', 'OPO-SHC9', 'OPO-SDY5', 'OPO-SKY1', 'TSNSBC4', 'TSNSJY4', 'TSN-SKA6',
     'MTO-SDJ4', 'MTO-SCR6', 'MTO-SDR3', 'MTO-SBW3', 'MTO-SHV1', 'MTO-JU8', 'MTO-SJB1'].freeze
